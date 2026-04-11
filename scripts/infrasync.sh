945bab23e405280d79e39eb28d53129d9af15426
rosin <rosin@protonmail.com>
2026-04-10 20:48:32 -0500

    move old scripts

---
+++ scripts/infrasync.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# Get git status of dotfiles
+# Check if remote has diverged, if so: abort
+# Show all changes, 
+# Add and commit all changes
+#
+# call dotfiles/reconcile.sh
+# call zetta/reconcile.sh
